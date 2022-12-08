119dd882c54f8bc750b76fa7c7a0bdb99c097dab
RodolfoFelixVazquez <70178092+RodolfoFelixVazquez@users.noreply.github.com>
2022-12-08 15:00:25 -0700

    Arreglo display del carrito

--- views/venta/Cart.php
+++ views/venta/Cart.php
@@ -1,4 +1,3 @@
-<?php include('../../includes/header.php')?>
 <?php 
     include('../../conexion/conn_localhost.php');
     $idsLibros = implode( ', ', array_values($_SESSION['libros']));
@@ -6,6 +5,8 @@
     $total = 0;
     $librosCarrito = mysqli_query($conn_localhost, $sql) or trigger_error("Login query failed");
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 <div class="container-fluid">
     <table class="table table-dark table-striped">
         <thead>
@@ -39,4 +40,9 @@
         </div>
     </div>
 </div>
-<?php include('../../includes/footer.php')?>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script>
+function refreshPage(){
+    window.location.href=window.location.href
+} 
